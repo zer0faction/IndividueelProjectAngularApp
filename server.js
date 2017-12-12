023761cd41c083fecde01985aceab02834b0d754
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2017-12-12 18:17:02 +0100

    heroku dingen

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express()
 
 app.use(express.static(__dirname+'/dist'));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
 const server = http.createServer(app);
 
-app.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080);
