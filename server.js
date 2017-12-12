e4027239708a1fc87f59c26d7c81cdbcd24606b2
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2017-12-12 18:33:44 +0100

    heroku dingen

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const app = express();
 const http = require('http');
 const path = require('path');
 
 const app = express()
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
